2bc04a258f831a665b6275cd2cade1a6b43ee836
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2025-04-15 11:24:27 -0300

    feat: finish character module and character component

---
+++ src/app/page.jsx
@@ -0,0 +1,16 @@
+import CharacterList from "../components/characterList";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <CharacterList />
+      </main>
+
+      <footer className={styles.footer}>
+        <p>Desenvolvido durante o curso de Desenvolvimento de Sistemas</p>
+      </footer>
+    </div>
+  );
+}
